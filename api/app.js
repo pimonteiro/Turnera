2f58db2a8718577b02804c4685c120b28e735a1b
Leonardo Neri <leonardo-neri>
2020-01-29 00:06:02 +0000

    upload file route

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const groups = require('./routes/groups');
 const groupsPosts = require('./routes/groupsPosts');
@@ -17,6 +18,7 @@ const usersPosts = require('./routes/usersPosts');
 const posts = require('./routes/posts');
 const auth = require('./routes/auth');
 const feed = require('./routes/feed');
+const upload = require('./routes/upload');
 const AuthController = require("./controllers/auth");
 
 const app = express();
@@ -26,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(fileUpload());
 
 app.use('/api', auth);
 
@@ -56,6 +59,7 @@ app.use('/api', usersGroups);
 app.use('/api', usersPosts);
 app.use('/api', posts);
 app.use('/api', feed);
+app.use('/api', upload);
 
 
 app.use(function(req, res, next) {
---
+++ api/routes/upload.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const { ClientSecretCredential } = require("@azure/identity");
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const account = "turnera";
+const defaultAzureCredential = new ClientSecretCredential(
+    '5633c12f-032f-4713-b960-416376fec2db',
+    '281a9491-4f98-4e72-a109-254841fc079e',
+    '@2AS.Kd7X//qrFGXPmw3sejUtC1Jai-q'
+    );
+
+const blobServiceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net`,
+    defaultAzureCredential
+);
+
+async function upload_file(data, blobName) {
+    const containerName = "files";
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    return "https://turnera.blob.core.windows.net/" + containerName + "/" + blobName
+}
+
+
+router.post('/upload', async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let file = req.files.file
+
+    upload_file(file.data, file.name)
+        .then(link => {
+            res.send(link)
+        })
+        .catch(err => {
+            res.jsonp(err)
+        })
+});
+
+module.exports = router;
