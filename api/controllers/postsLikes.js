a2079aacc0823395b5fb8b8ff69692e86725dc94
Leonardo Neri <leonardo-neri>
2020-01-29 10:25:51 +0000

    fix multiple likes by sigle user on post

--- api/controllers/postsLikes.js
+++ api/controllers/postsLikes.js
@@ -3,7 +3,7 @@ const PostLikes = module.exports;
 PostLikes.createPostLike = (session, req, res) => {
   session.run(
     "MATCH (p:Post { id: $pid }), (u:User { id: $uid}) \
-    CREATE (u)-[:likes]->(p)",
+    MERGE (u)-[:likes]->(p)",
     { pid: req.params.id, uid: req.body.user }
     )
     .then(() => {
