0817520d87f2c78f2e0f671d11eca77d84c1e904
Leonardo Neri <leonardo-neri>
2020-01-29 10:08:58 +0000

    post owner on feed

--- api/controllers/feedController.js
+++ api/controllers/feedController.js
@@ -3,13 +3,13 @@ const FeedController = module.exports;
 FeedController.index = (session, req, res) => {
     session.run(
         "MATCH (u:User { id: $id })-[:friend]-(f:User)-[:post]-(p:Post) \
-        RETURN p as post \
+        RETURN p as post, f as owner \
         UNION ALL \
-        MATCH (u:User { id: $id })-[:member]-(g:Group)-[:post]-(p:Post) \
-        RETURN p as post \
+        MATCH (u:User { id: $id })-[:member]-(g:Group)-[:post]-(p:Post)-[:post]-(f:User) \
+        RETURN p as post, f as owner \
         UNION ALL \
         MATCH (u:User { id: $id })-[:post]-(p:Post) \
-        RETURN p as post"
+        RETURN p as post, u as owner"
         ,
         { id: req.params.id }
         )
@@ -17,7 +17,12 @@ FeedController.index = (session, req, res) => {
             let ret = []
 
             data.records.forEach(record => {
-                ret.push(record.get('post').properties)
+                let post = {}
+
+                post = record.get('post').properties
+                post.owner = record.get('owner').properties
+
+                ret.push(post)
             })
 
             res.jsonp(ret)
